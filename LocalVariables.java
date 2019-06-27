6c10ace4b377746df80d528b2250e8391f881c8b
Falseille <37417596+Falseille@users.noreply.github.com>
2019-06-27 22:16:04 +0200

    Add files via upload

---
+++ LocalVariables.java
@@ -0,0 +1,39 @@
+package com.LocalVariables;
+
+public class LocalVariables {
+
+    public int notValid() {
+        int y = 10;
+        int x;
+        int reply = x + y;
+        return reply;
+        System.out.println(reply);
+    }
+
+    public int Valid () {
+        int y = 10;
+        int x;
+        x = 3; //  This one is initialized, because there is a value for X.
+        int replyValid = x + y;
+        return  replyValid;
+        System.out.println(replyValid);
+        // This cannot be printed out because of the statement return, cannot complete it normally because it is not followed by a return value.
+        // It is in a infinite loop in this code of block.
+    }
+
+    public void findAnswer (boolean check) {
+        int answer;
+        int onlyOneBranch;
+        if (check) {
+            onlyOneBranch = 1;
+            answer =1;
+        }
+        else {
+            answer = 2;
+        } //The compiler is happy for the initializing because answer is initialized and will compile.
+        // onlyOneBranch will not compile because it is not initialized.
+        System.out.println (answer);
+        System.out.println(onlyOneBranch);
+    }
+}
+
