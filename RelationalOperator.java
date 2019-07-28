2d41ca3113ffa401b82f6dde40d760b1b158b899
Falseille <37417596+Falseille@users.noreply.github.com>
2019-07-28 16:53:17 +0200

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,18 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int x = 10, y = 20, z = 10;
+        System.out.println(x < y);
+        System.out.println(x <= y);
+        System.out.println(x >= z);
+        System.out.println(x > z);
+        /* The first will give a answer true because x is less to y.
+        second will give also a answer true because x is less or equal to y
+        third will give also a answer true because x is grater or equal to z
+        fourth will give a answer false because its not greater then z.
+         */
+
+
+    }
+}
+
+
